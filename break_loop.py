4569de42b34ab253c5d0076d43a5b9ed61facbc5
Valery Hoyos <73263344+valery-22@users.noreply.github.com>
2025-07-08 22:26:49 -0500

    Add files via upload

---
+++ break_loop.py
@@ -0,0 +1,24 @@
+<<<<<<< HEAD
+# We'll check a series of temperatures and print a message accordingly
+temperatures = [18, 22, 30, 35]
+
+for temp in temperatures:
+    if temp < 20:
+        print('The temperature is below 20 degrees.')
+        break
+    elif temp > 25:
+        print('It is warm outside.')
+        continue
+=======
+# We'll check a series of temperatures and print a message accordingly
+temperatures = [18, 22, 30, 35]
+
+for temp in temperatures:
+    if temp < 20:
+        print('The temperature is below 20 degrees.')
+        break
+    elif temp > 25:
+        print('It is warm outside.')
+        continue
+>>>>>>> b76c48a (Initial commit)
+    print('Might be chilly, dress appropriately.')
